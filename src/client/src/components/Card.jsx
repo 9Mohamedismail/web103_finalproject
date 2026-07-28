2fe7ce32b38efebdc0465a77fd43f7e735f5a293
PotatoCodo <aquach1190@gmail.com>
2026-07-28 02:22:08 -0400

    discover page car fetching

---
+++ src/client/src/components/Card.jsx
@@ -0,0 +1,6 @@
+//do later
+function Card() {
+  return <h1>bleh</h1>;
+}
+
+export default Card;
--- src/client/src/pages/DiscoverPage.jsx
+++ src/client/src/pages/DiscoverPage.jsx
@@ -1,4 +1,3 @@
-import { response } from "express";
 import { useEffect, useState } from "react";
 import { data } from "react-router-dom";
 
@@ -7,25 +6,35 @@ const API_URL = "http://localhost:3001";
 
 function DiscoverPage() {
   const [cards, setCards] = useState([]);
+  const [loading, isLoading] = useState(true);
 
   useEffect(() => {
     async function fetchCards() {
       const response = await fetch(`${API_URL}/api/cards`);
       const data = await response.json();
 
-      console.log(response);
-      console.log(data);
-      setCards(data);
+      console.log(data.data);
+      setCards(data.data);
+      isLoading(false);
     }
 
     fetchCards();
   }, []);
 
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <>
-      <div>
-        <h1>hi</h1>
-      </div>
+      <h1>Find Your Perfect Card</h1>
+      {cards.map((card) => (
+        <div>
+          <h3>{card.name}</h3>
+          <p>{card.issuers.name}</p>
+          <p>${card.annual_fee}</p>
+          <p>Signup bonus: {card.signup_bonus}</p>
+        </div>
+      ))}
     </>
   );
 }
